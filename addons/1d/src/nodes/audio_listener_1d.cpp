5d80c511bc8ceef35336d6d1ec9026cf189ed33c
Aaron Franke <arnfranke@yahoo.com>
2022-10-16 15:37:22 -0500

    [WIP] Port to GDExtension

---
+++ addons/1d/src/nodes/audio_listener_1d.cpp
@@ -0,0 +1,39 @@
+#include "audio_listener_1d.h"
+
+#include <godot_cpp/classes/audio_listener2d.hpp>
+
+void AudioListener1D::set_current(const bool p_current) {
+	if (p_current) {
+		_real_listener->make_current();
+	} else {
+		_real_listener->clear_current();
+	}
+}
+
+bool AudioListener1D::is_current() const {
+	return _real_listener->is_current();
+}
+
+void AudioListener1D::set_is_vertical(const bool p_is_vertical) {
+	_is_vertical = p_is_vertical;
+}
+
+bool AudioListener1D::get_is_vertical() const {
+	return _is_vertical;
+}
+
+void AudioListener1D::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_current", "current"), &AudioListener1D::set_current);
+	ClassDB::bind_method(D_METHOD("is_current"), &AudioListener1D::is_current);
+	ClassDB::bind_method(D_METHOD("set_is_vertical", "is_vertical"), &AudioListener1D::set_is_vertical);
+	ClassDB::bind_method(D_METHOD("get_is_vertical"), &AudioListener1D::get_is_vertical);
+
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "current"), "set_current", "is_current");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "is_vertical"), "set_is_vertical", "get_is_vertical");
+}
+
+AudioListener1D::AudioListener1D() {
+	_real_listener = memnew(AudioListener2D);
+	add_child(_real_listener);
+	_real_listener->make_current();
+}
---
+++ addons/1d/src/nodes/audio_listener_1d.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "node_1d.h"
+
+namespace godot {
+class AudioListener2D;
+}
+
+using namespace godot;
+
+class AudioListener1D : public Node1D {
+	GDCLASS(AudioListener1D, Node1D);
+
+	// Viewport is hard-coded to work with AudioListener2D (and
+	// AudioListener3D). We'll just use AudioListener2D as a
+	// proxy so that we don't have to modify Viewport.
+	AudioListener2D *_real_listener;
+	bool _is_vertical = false;
+
+protected:
+	static void _bind_methods();
+
+public:
+	void set_current(const bool p_current);
+	bool is_current() const;
+
+	void set_is_vertical(const bool p_is_vertical);
+	bool get_is_vertical() const;
+
+	AudioListener1D();
+};
